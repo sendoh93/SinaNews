29e90d9dcc4df47e0635b577295d0122df025131
hztataypf <474341867@qq.com>
2017-02-07 01:15:18 +0800

    test

--- app/src/main/java/com/tcs/sinanews/MyApplication.java
+++ app/src/main/java/com/tcs/sinanews/MyApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 
+import cn.jpush.android.api.JPushInterface;
+
 /**
  * Created by pingfan.yang on 2017/1/3.
  */
@@ -11,6 +13,9 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(this);
         Fresco.initialize(this);
+
     }
 }
