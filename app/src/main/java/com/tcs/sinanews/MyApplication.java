3e6690e9502721a401c059be1c7b16cb00b2e3af
hztataypf <474341867@qq.com>
2017-02-16 23:08:28 +0800

    666

--- app/src/main/java/com/tcs/sinanews/MyApplication.java
+++ app/src/main/java/com/tcs/sinanews/MyApplication.java
@@ -1,10 +1,14 @@
 package com.tcs.sinanews;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 
+import java.util.Set;
+
 import cn.jpush.android.api.JPushInterface;
+import cn.jpush.android.api.TagAliasCallback;
 
 /**
  * Created by pingfan.yang on 2017/1/3.
@@ -18,4 +22,5 @@ public class MyApplication extends Application {
         Fresco.initialize(this);
 
     }
+
 }
--- app/src/main/java/com/tcs/sinanews/ui/activity/WelcomeActivity.java
+++ app/src/main/java/com/tcs/sinanews/ui/activity/WelcomeActivity.java
@@ -1,6 +1,7 @@
 package com.tcs.sinanews.ui.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
@@ -8,10 +9,13 @@ import android.widget.LinearLayout;
 
 import com.tcs.sinanews.R;
 
+import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import butterknife.Bind;
+import cn.jpush.android.api.JPushInterface;
+import cn.jpush.android.api.TagAliasCallback;
 
 /**
  * Created by Administrator on 2017/2/2.
@@ -56,6 +60,18 @@ public class WelcomeActivity extends BaseActivity {
     }
 
     private void initView() {
+        String Rid = JPushInterface.getRegistrationID(this);
+        Log.e("jiguang---","Rid ===="+Rid);
+        JPushInterface.setAlias(this, "ypfAndroid", new TagAliasCallback() {
+            @Override
+            public void gotResult(int i, String s, Set<String> set) {
+                Log.e("jiguang-------","Alias :" + i);
+                if (i == 0)
+                {
+                    Log.e("jiguang------","设置别名成功咯");
+                }
+            }
+        });
         Animation animotion = new ScaleAnimation(1f,1f,1.2f,1f,0.5f,0.5f);
         animotion.setDuration(2000);
         mLlBackground.clearAnimation();
