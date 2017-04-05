e41880b6b1c62d4ed70f3c2540c3bad331df6805
hztataypf <474341867@qq.com>
2017-04-06 02:49:38 +0800

    修改

---
+++ easeui/simpledemo/src/com/hyphenate/easeuisimpledemo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.hyphenate.easeuisimpledemo;
+
+import android.app.Application;
+
+import com.hyphenate.easeui.controller.EaseUI;
+
+public class DemoApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        EaseUI.getInstance().init(this, null);
+    }
+    
+}
